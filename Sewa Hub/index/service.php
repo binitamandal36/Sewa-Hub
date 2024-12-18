5996845aa4a7227ed7f3d8e15af5b6881fb4816b
Binita Kumari Mandal <160568166+binitamandal36@users.noreply.github.com>
2024-12-18 08:32:13 +0545

    Add files via upload

---
+++ Sewa Hub/index/service.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Service</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <!----------------------------------------- Service Page ------------------------------------------------------>
+    <div id="Servicepage"> 
+        <!-- ALL content of service page -->
+        <div id="serviceimg"> 
+            <!-- front image/text of service page  -->
+            <img src="uploads/clean.jpeg" alt="Cleaning of your house">
+            <h1 style="text-align: center">S<br>E<br>W<br>A<br><br>H<br>U<br>B</h1>
+            <p style="text-align: center">Y<br>O<br>U<br>R<br>
+            <br>ALL<br>
+            <br>T<br>I<br>M<br>E<br>
+            <br>S<br>O<br>L<br>U<br>T<br>I<br>O<br>N<br>
+            <br>HUB</P>
+        </div>
+
+        <div id="servicecontent">
+            <!-- service content  -->
+        </div>
+
+        <div id="footer">
+            <!-- footer of the website -->
+        </div>
+    </div>
+</body>
+</html>
